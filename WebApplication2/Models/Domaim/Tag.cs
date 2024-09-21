48c3548c580f269b74de5280be213214aaab04a5
NoviMar <141076996+NoviMar@users.noreply.github.com>
2024-09-21 13:39:50 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Models/Domaim/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication2.Models.Domaim
+{
+
+        public class Tag
+        {
+            public Guid Id { get; set; }
+            public int MyProperty { get; set; }
+            public int MyProperty1 { get; set; }
+            public ICollection<BlogPost> BlogPosts { get; set; }
+        }
+ 
+
+}
