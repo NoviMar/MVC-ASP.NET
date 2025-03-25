a2712b6751a055f8298ad049093ba1518624ff4a
Maria <141076996+NoviMar@users.noreply.github.com>
2025-03-25 13:38:27 +0300

    cleaning the repository

--- WebApplication2/WebApplication2/Models/ViewModels/AddTagRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication2.Models.ViewModels
-{
-    public class AddTagRequest
-    {
-        public string Name { get; set; }
-        public string DisplayName { get; set; }
-    }
-}
